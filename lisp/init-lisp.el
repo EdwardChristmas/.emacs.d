3b14a185f7abe52c395cd6071f7ed5d4b39cae41
c4droid <c4droid@foxmail.com>
2020-02-04 20:41:27 +0800

    Add paredit

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -2,4 +2,10 @@
   :ensure nil
   :defer t
   :bind ("C-c C-b" . eval-buffer))
+(use-package paredit
+  :defer t
+  :hook ((emacs-lisp-mode . enable-paredit-mode)
+         (lisp-mode . enable-paredit-mode)
+         (lisp-interaction-mode . enable-paredit-mode)
+         (ielm-mode . enable-paredit-mode)))
 (provide 'init-lisp)
