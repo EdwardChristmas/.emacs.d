0ee33e544fb34b51fc0636973370a1a596e9a38a
c4droid <c4droid@foxmail.com>
2020-02-07 19:03:50 +0800

    Add some go-mode keybinding and setup gofmt command

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,3 +1,9 @@
 (use-package go-mode
-  :defer t)
+  :defer t
+  :bind (:map go-mode-map
+         ("M-," . godef-jump)
+         ("M-." . pop-tag-mark)
+         ("C-c C-f" . gofmt))
+  :config
+  (setq gofmt-command "goimports"))
 (provide 'init-golang)
